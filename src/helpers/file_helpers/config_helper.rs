37112ce320e4ab1fb11bbcc26633ab34c66840dd
James Jilhaney <notail2111@gmail.com>
2023-04-10 23:06:10 +0800

    Small changes

--- src/helpers/file_helpers/config_helper.rs
+++ src/helpers/file_helpers/config_helper.rs
@@ -1,7 +1,7 @@
-use std::fs::File;
-use std::io::{self, Error, ErrorKind, Read};
+use std::fs::{self, File, OpenOptions};
+use std::io::{self, Error, ErrorKind, Read, Write};
 // use toml::Table;
-// use toml::Value;
+use toml::Value;
 
 pub fn get_config() -> Result<File, io::Error> {
     let config_file = File::open("config.toml")?;
@@ -20,11 +20,24 @@ pub fn create_config() -> Result<String, io::Error> {
                 return Err(Error::new(ErrorKind::PermissionDenied, "Permission denied"));
             }
             _ => {
-                return Err(Error::new(ErrorKind::Other, "Unknown error: {err}"));
+                return Err(Error::new(ErrorKind::Other, err));
             }
         },
         Ok(_) => {
             println!("Creating config file");
+            let mut toml_obj = toml::map::Map::new();
+
+            toml_obj.insert("PRIVATE_KEY".to_string(), Value::String("".to_string()));
+
+            let new_config_ref = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .open("config.toml")?;
+
+            let toml_string = toml::to_string(&toml_obj).unwrap();
+
+            fs::write("config.toml", toml_string)?;
+
             return Ok("Successfully created config file".to_owned());
         }
     }
