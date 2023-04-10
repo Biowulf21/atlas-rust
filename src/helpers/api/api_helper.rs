618f1853ac627221934224214eaaca91b72f9ef0
James Jilhaney <notail2111@gmail.com>
2023-04-10 21:19:41 +0800

    Added boilerplate code for api requesting functions

--- src/helpers/api/api_helper.rs
+++ src/helpers/api/api_helper.rs
@@ -1,4 +1,4 @@
-pub fn get_students_by_date() -> Option<i32> {
+pub fn get_students_by_date(date: String) -> Option<i32> {
     return Some(3);
 }
 
@@ -6,3 +6,10 @@ pub fn get_students_by_date_range() -> Option<i32> {
     return Some(3);
 }
 
+pub fn get_students_tomorrow() -> Option<i32> {
+    return Some(3);
+}
+
+pub fn get_students_today() -> Option<i32> {
+    return Some(3);
+}
