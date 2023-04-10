a7f51c21697814a3defc16fd646f697c91b6474c
James Jilhaney <notail2111@gmail.com>
2023-04-10 21:20:12 +0800

    Added init file and function

--- src/init/init.rs
+++ src/init/init.rs
@@ -1,1 +1,18 @@
-pub fn init() -> Result() {}
+use crate::helpers::file_helpers::config_helper;
+
+pub fn init_app() {
+    let contents_or_error = config_helper::get_config();
+
+    let finding_config_result = match contents_or_error {
+        Ok(file) => {
+            println!("Found config file. Proceeding.");
+            println!("{:?}", file);
+            file;
+        }
+        Err(err) => {
+            println!("Config file not found. Creating one.");
+            let creation_result = config_helper::create_config();
+        }
+    };
+    println!("{:?}", finding_config_result);
+}
