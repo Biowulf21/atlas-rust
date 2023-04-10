7270fada5a4881787599931c0332c126900b1380
James Jilhaney <notail2111@gmail.com>
2023-04-10 23:06:32 +0800

    Renamed project in use statement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+#[allow(warnings)]
 // use atlas_v2::helpers::api::api_helper;
-use atlas_v2::init::init::init_app;
+use atlas_rust::init::init::init_app;
 
 fn main() {
     println!("Hello, world!");
